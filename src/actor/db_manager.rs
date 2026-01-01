1ec92a9aa5afb46a65a73c5a7459db4b3661978c
zaigiaz <shayne.tman@gmail.com>
2026-01-01 16:13:35 -0600

    finished db functions

--- src/actor/db_manager.rs
+++ src/actor/db_manager.rs
@@ -3,8 +3,6 @@ use std::error::Error;
 use crate::actor::crawler::FileMeta;
 
 
-
-
 const BATCH_SIZE: usize = 1;
 
 pub async fn run(actor: SteadyActorShadow, 
@@ -22,15 +20,16 @@ async fn internal_behavior<A: SteadyActor>(mut actor: A,
 
     // TODO: example code that I need to change
     let db: sled::Db = sled::open("../db").unwrap();
-    let _ = db.insert(b"yo!", b"v1");
-
+    let ctr: i32 = 0;
 
     while actor.is_running(|| crawler_rx.is_closed_and_empty()) {
 
 	actor.wait_avail(&mut crawler_rx, BATCH_SIZE).await;
 	let recieved = actor.try_take(&mut crawler_rx);
 
-	recieved.expect("expected FileMeta Struct (db_actor)").meta_print();
+	let msg = recieved.expect("expected FileMeta Struct (db_actor)");
+	let _ = db_add(ctr, msg.clone(), db.clone());
+	msg.meta_print();
 	}
 
   Ok(())
@@ -54,13 +53,11 @@ Ok(())
 
 
 // edit db entry given key
-fn db_edit(key: i32) -> Result<(), Box<dyn Error>> {
-
-    let key_s = key.to_be_bytes();
+fn db_edit(key: i32, value: FileMeta, db: sled::Db) -> Result<(), Box<dyn Error>> {
 
     // sled has immutable db, so we need to delete old key then insert new
-
-    // let _ = db
+    let _ = db_remove(key, db.clone());
+    let _ = db_add(key, value, db.clone());
 
 Ok(())
 }
@@ -72,11 +69,7 @@ fn db_remove(key: i32, db: sled::Db) -> Result<(), Box<dyn Error>> {
     let key_s = key.to_be_bytes();
     
     // remove entry based on key
-    db.remove(key_s);
+    let _ = db.remove(key_s);
 
     Ok(())
 }
-
-
-
-
